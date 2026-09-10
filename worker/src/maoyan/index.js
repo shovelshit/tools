905de08f0229b183c7f499565163d936c0a9d01a
lijinfeng <728729140@qq.com>
2026-09-11 03:08:38 +0800

    fix: maoyan/index.js 出口对齐 parseBatchMinutes, 修复构建

--- worker/src/maoyan/index.js
+++ worker/src/maoyan/index.js
@@ -4,6 +4,6 @@ export { CITY_LIST } from "./cities.js";
 export { fetchCinemaDetail, searchCinemasByKw } from "./api.js";
 export { runCheck } from "./check.js";
 export { currentChannel, pushNotify } from "./notify.js";
-export { cronBatchMinutes, describeCron, isMinuteStepCron, resolveCronExprs, minBatchMinutes, describeCrons, isMinuteStepCrons, CRON_EXPRESSION } from "./cron.js";
+export { parseBatchMinutes, describeCron, isMinuteStepCron, resolveCronExprs, minBatchMinutes, describeCrons, isMinuteStepCrons, CRON_EXPRESSION } from "./cron.js";
 export { MONITOR_DDL_DAYS, ddlFromNow, isExpired } from "./ddl.js";
 export { checkAuthFull, syncCronTokens, handleAdminTokens, runScheduledChecks } from "./tokens.js";
